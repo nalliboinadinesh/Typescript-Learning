8ab7a3b221aee6696e26a18c995bb0f2d8306ae4
Dinesh756 <nalliboinadinesh@gmail.com>
2026-03-03 12:25:07 +0530

    interfaces

--- dist/index.js
+++ dist/index.js
@@ -29,4 +29,78 @@ const userInfo = (name, city) => {
     return { name, city };
 };
 console.log(userInfo("dinesh", "Hyderabad"));
+class Bank {
+    accountNumber;
+    ownerName;
+    _balance;
+    constructor(accountNumber, ownerName, balance) {
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this._balance = balance;
+    }
+    get balance() {
+        return this._balance;
+    }
+    set balance(amount) {
+        if (amount < 0) {
+            console.log("Balance cannot be negative");
+        }
+        else {
+            this._balance = amount;
+        }
+    }
+}
+const myAccount = new Bank(123456, "Dinesh", 1000);
+console.log(myAccount.balance);
+const niat1 = {
+    name: "Dinesh",
+    NiatID: 123,
+    section: "A"
+};
+console.log(niat1);
+function doubleAge({ age, name }) {
+    return { doubleAge: age * 2 };
+}
+console.log(doubleAge({ age: 20, name: "Dinesh" }));
+const add = (a, b) => a + b;
+console.log(add(1, 2));
+const users = [
+    { name: "Dinesh", age: 20 },
+    { name: "Nalliboina", age: 25 },
+];
+console.log(users);
+const myStringArray = ["Dinesh", "Nalliboina"];
+const userProfile = {
+    users: [
+        { name: "Dinesh", age: 20, email: "dinesh@example.com" },
+        { name: "Nalliboina", age: 25 },
+    ],
+};
+console.log(userProfile.users[0]);
+class SavingsAccount {
+    accountNumber;
+    ownerName;
+    balance;
+    constructor(accountNumber, ownerName, balance) {
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.balance = balance;
+    }
+    deposit(amount) {
+        this.balance += amount;
+    }
+    withdraw(amount) {
+        if (amount > this.balance) {
+            console.log("Insufficient funds");
+        }
+        else {
+            this.balance -= amount;
+        }
+    }
+}
+const mySavingsAccount = new SavingsAccount(123456, "Dinesh", 1000);
+mySavingsAccount.deposit(500);
+console.log(mySavingsAccount.balance);
+mySavingsAccount.withdraw(200);
+console.log(mySavingsAccount.balance);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -49,3 +49,149 @@ const userInfo = (
 };
 
 console.log(userInfo("dinesh", "Hyderabad"));
+
+
+// classes and access modifiers
+
+class Bank {
+  accountNumber: number;  
+  ownerName: string;
+  private _balance: number;
+  constructor(accountNumber: number, ownerName: string, balance: number) {
+    this.accountNumber = accountNumber;
+    this.ownerName = ownerName;
+    this._balance = balance;
+  }
+  get balance(): number {
+    return this._balance;
+  }
+  set balance(amount: number) {
+    if (amount < 0) {
+      console.log("Balance cannot be negative");
+    } else {
+      this._balance = amount;
+    }   
+  }
+}
+
+const myAccount = new Bank(123456, "Dinesh", 1000);
+console.log(myAccount.balance);
+
+
+//interface object 
+
+interface Niat{
+  name: string;
+  NiatID: number;
+  section: string;
+}
+
+const niat1: Niat = {
+  name: "Dinesh",
+  NiatID: 123,
+  section: "A"
+};
+console.log(niat1);
+
+// interface function
+
+interface ageDoubler {
+  age: number;
+  name:string;
+}
+interface ReturnType {
+  doubleAge: number;
+}
+
+function doubleAge({age,name}: ageDoubler): ReturnType {
+  return { doubleAge: age * 2 };
+}
+
+console.log(doubleAge({age: 20, name: "Dinesh"}));
+
+interface addition {
+  (a: number, b: number): number;
+}
+const add: addition = (a, b) => a + b;
+console.log(add(1, 2));
+
+// interface in arrray
+
+interface User {
+  name: string;
+  age: number;
+}
+
+const users: User[] = [
+  { name: "Dinesh", age: 20 },
+  { name: "Nalliboina", age: 25 },
+];
+console.log(users);
+
+interface stringArray {
+
+  [index: number]: string;
+}
+const myStringArray: stringArray = ["Dinesh", "Nalliboina"];
+
+
+//Inline object array interface
+
+interface UserProfile {
+  users:{
+    name: string;
+    age: number;
+    email?: string; // optional property  
+  }[];
+}
+
+const userProfile: UserProfile = {
+  users: [
+    { name: "Dinesh", age: 20, email: "dinesh@example.com" },
+    { name: "Nalliboina", age: 25 },
+  ],
+};
+console.log(userProfile.users[0]);
+
+
+// interface with class
+
+
+interface BankAccount {
+  accountNumber: number;
+  ownerName: string;
+  balance: number;
+  deposit(amount: number): void;
+  withdraw(amount: number): void;
+}
+
+class SavingsAccount implements BankAccount {
+  accountNumber: number;
+  ownerName: string
+  balance: number;
+  constructor(accountNumber: number, ownerName: string, balance: number) {
+    this.accountNumber = accountNumber;
+    this.ownerName = ownerName;
+    this.balance = balance;
+  }
+  deposit(amount: number): void {
+    this.balance += amount;
+  }
+  withdraw(amount: number): void {
+    if (amount > this.balance) {
+      console.log("Insufficient funds");
+    }
+    else {
+
+      this.balance -= amount;
+    } 
+
+  }
+
+}
+
+const mySavingsAccount = new SavingsAccount(123456, "Dinesh", 1000);
+mySavingsAccount.deposit(500);
+console.log(mySavingsAccount.balance);
+mySavingsAccount.withdraw(200);
+console.log(mySavingsAccount.balance);
